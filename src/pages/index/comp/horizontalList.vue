766ebda1e4565382ead5b7aa2e78de2bb80c15a1
nero.wang <bakujin@qq.com>
2021-12-22 17:26:38 +0800

    feat: 首页增加横通、滚动

--- src/pages/index/comp/horizontalList.vue
+++ src/pages/index/comp/horizontalList.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="py-16">
-    <div class="mx-32 flex">
+    <div class="mx-32 pb-8 flex">
       <span>本周畅销</span>
-      <span class="text-sm">本周畅销</span>
-      <span class="text-sm">查看更多</span>
+      <span class="text-sm flex-grow ml-16 pt-8 text-gray">本周畅销</span>
+      <span class="text-sm pt-8 text-gray">查看更多 ></span>
     </div>
     <u-scroll-list>
       <div v-for="(item, index) in params.list" :key="index" class="item my-8 text-center">
         <image class="icon rounded-sm" mode="aspectFill" :src="item.src"></image>
-        <div class="text-sm text-left"> 九妹三大数据库纳米颗粒，上课啦那款吗看了吗{{ index }} </div>
+        <div class="title text-sm text-left truncate"> 九妹三大数据库纳米颗粒，上课啦那款吗看了吗{{ index }} </div>
       </div>
     </u-scroll-list>
   </div>
@@ -35,5 +35,9 @@ export default {
 .icon {
   width: 160rpx;
   height: 160rpx;
+}
+.title {
+  width: 160rpx;
+  margin: 0 auto;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <u-navbar safe-area-inset-top placeholder fixed bg-color="#F2F3F7">
-      <div slot="left" class="px-16 py-16">
-        <u-search placeholder="请输入关键字" :show-action="false" disabled></u-search>
+      <div slot="left" class="pl-8 py-16 flex text-sm">
+        <span class="icon-c mr-16"> &#xe6ac; 南京</span>
+        <u-search placeholder="请输入关键字" :show-action="false" bg-color="#f8f8f8" disabled></u-search>
       </div>
     </u-navbar>
 
@@ -18,6 +19,48 @@
       <horizontalList :params="p2" />
     </div>
 
+    <div class="m-24">
+      <flow :params="p" />
+    </div>
+
+    <u-tabs
+      scrollable
+      :list="[
+        {
+          name: '关注阿萨',
+        },
+        {
+          name: '推阿萨荐',
+        },
+        {
+          name: '电阿萨影',
+        },
+        {
+          name: '科阿萨技',
+        },
+        {
+          name: '音乐阿萨',
+        },
+        {
+          name: '美食阿萨',
+        },
+        {
+          name: '文化阿萨',
+        },
+        {
+          name: '财经阿萨',
+        },
+        {
+          name: '手工阿萨',
+        },
+      ]"
+    ></u-tabs>
+    <scroll-view scroll-y scroll-with-animation>
+      <div v-for="i in 8" :key="i" class="m-24 bg-white rounded">
+        <item small is-item>{{ i }}</item>
+      </div>
+    </scroll-view>
+
     <qx-tabbar />
   </div>
 </template>
@@ -25,15 +68,21 @@
 import banner from './comp/banner.vue'
 import diamond from './comp/diamond.vue'
 import horizontalList from './comp/horizontalList.vue'
+import flow from './comp/flow.vue'
 import { section } from './comp/meta'
+import item from '../cart/comp/cartItem.vue'
 
 var p2 = section()
 p2.list.push(...p2.list)
+p2.height = '120rpx'
+
 export default {
   components: {
     banner,
     diamond,
     horizontalList,
+    flow,
+    item,
   },
   data() {
     return {
@@ -48,3 +97,8 @@ export default {
   methods: {},
 }
 </script>
+<style scoped>
+.icon-c {
+  line-height: 64rpx;
+}
+</style>
